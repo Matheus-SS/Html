549a639e406b1ef7ec351f734bfcb38ee86a8e95
matheus <matheus_sa_santos@hotmail.com>
2018-08-02 15:35:19 -0300

    applayers

---
+++ applayers/script.js
@@ -0,0 +1,13 @@
+function ativar(){
+			if (document.getElementById("l").style.display=='block') {
+				document.getElementById("l").style.display='none';
+			}else{
+				document.getElementById("l").style.display='block';
+			}
+		}
+
+function linkar(){
+	if (document.getElementById("l").style.display=='block') {
+		document.getElementById("l").style.display='none';
+		}
+}
